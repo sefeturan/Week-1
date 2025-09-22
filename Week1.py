237655a436527228f21ea34c40c9e4295bc56068
sefeturan <seref.turan04@gmail.com>
2025-09-22 14:51:45 +0300

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,37 @@
+class plane:
+    def __init__(self, position, id):
+        self.position = (0,0,0)
+        self.id = id
+
+    def begin_fly(self, position):
+            self.position = position
+
+    def end_fly(self, position):
+                self.position = position
+
+    def go_forward(self,position):
+        self.position=position
+
+
+    def show(self):
+                print('plane {}, coordinates on {} now'.format(self.id,self.position))
+
+ucak1 = plane('u1', (0, 0, 0))
+siha = plane('s1',(0,0,0))
+iha = plane('i1',(0,0,0))
+
+
+class filo:
+    def __init__(self):
+        self.planes=[]
+    def show(self):
+        for i in self.planes:
+            print('our filo included {}'.format(i.id))
+    def add_filo(self,plane):
+                self.planes.append(plane)
+
+
+filo1=filo()
+
+
+
